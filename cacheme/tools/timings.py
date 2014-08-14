3ca022bcbec7955cae8476c2e69cefc5a8916949
Jeff Trawick <trawick@gmail.com>
2014-08-14 10:42:22 -0400

    just time cache lookup

--- cacheme/tools/timings.py
+++ cacheme/tools/timings.py
@@ -11,14 +11,18 @@ global_test_data = None
 global_api_name = None
 
 
-def time_cache(name):
+def cache_set(name):
     fns = data.APIS[name]
     fns[0]('mykey', global_test_data)
+
+
+def time_cache_lookup(name):
+    fns = data.APIS[name]
     got_back = fns[1]('mykey')
     assert got_back == global_test_data
 
 
-def timings(count):
+def lookup_timings(count):
     global global_api_name, global_test_data
 
     global_test_data = data.build(num_outer=1000, num_inner=1000)
@@ -27,14 +31,15 @@ def timings(count):
         #  MemcachedCache: may need extra config to support large objects
         #  DatabaseCache: too slow
         #  LocMemCache: need something shared between processes
-        if global_api_name in ['MemcachedCache', 'DatabaseCache', 'LocMemCache']:
+        if global_api_name in ['MemcachedCache', 'DatabaseCache']:
             continue
         if global_api_name is not None:
             printable = global_api_name
         else:
             printable = 'None'
-        print '%-20s:' % printable, timeit.timeit('time_cache(global_api_name)', number=count,
-                                                  setup="from __main__ import time_cache, global_api_name")
+        cache_set(global_api_name)
+        print '%-20s:' % printable, timeit.timeit('time_cache_lookup(global_api_name)', number=count,
+                                                  setup="from __main__ import time_cache_lookup, global_api_name")
 
 
 def process(args):
@@ -46,7 +51,7 @@ def process(args):
     except ValueError:
         print >> sys.stderr, 'Please check the number of iterations'
         sys.exit(1)
-    timings(count)
+    lookup_timings(count)
 
 if __name__ == '__main__':
     process(sys.argv)
